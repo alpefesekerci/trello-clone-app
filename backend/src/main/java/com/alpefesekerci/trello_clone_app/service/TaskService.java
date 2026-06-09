c367222bd66dd79bd862aa87c0a2ed71452bc45c
Alp Efe Şekerci <alpefesekerci@gmail.com>
2026-06-09 13:52:13 +0300

    feat: implement TaskService with basic CRUD operations

--- backend/src/main/java/com/alpefesekerci/trello_clone_app/service/TaskService.java
+++ backend/src/main/java/com/alpefesekerci/trello_clone_app/service/TaskService.java
@@ -1,4 +1,16 @@
 package com.alpefesekerci.trello_clone_app.service;
 
-public class TaskService {
+import com.alpefesekerci.trello_clone_app.entity.Task;
+import java.util.List;
+
+public interface TaskService {
+    Task createTask(Task task);
+
+    List<Task> getAllTasks();
+
+    Task getTaskById(Long id);
+
+    Task updateTask(Long id, Task taskDetails);
+
+    void deleteTask(Long id);
 }
--- backend/src/main/java/com/alpefesekerci/trello_clone_app/service/impl/TaskServiceImpl.java
+++ backend/src/main/java/com/alpefesekerci/trello_clone_app/service/impl/TaskServiceImpl.java
@@ -1,4 +1,51 @@
 package com.alpefesekerci.trello_clone_app.service.impl;
 
-public class TaskServiceImpl {
+import org.springframework.stereotype.Service;
+import com.alpefesekerci.trello_clone_app.service.TaskService;
+import lombok.RequiredArgsConstructor;
+import com.alpefesekerci.trello_clone_app.repository.TaskRepository;
+import com.alpefesekerci.trello_clone_app.entity.Task;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+
+public class TaskServiceImpl implements TaskService {
+     private final TaskRepository taskRepository;
+
+     @Override
+    public Task createTask(Task task) {
+         return taskRepository.save(task);
+     }
+
+     @Override
+     public List<Task> getAllTasks() {
+         return taskRepository.findAll();
+     }
+
+     @Override
+     public Task getTaskById(Long id) {
+         return taskRepository.findById(id)
+                 .orElseThrow(() -> new RuntimeException("Görev bulunamadı! ID: " + id));
+     }
+
+     @Override
+    public Task updateTask(Long id, Task taskDetails) {
+         Task existingTask = taskRepository.findById(id)
+                 .orElseThrow(() -> new RuntimeException("Görev bulunamadı! ID: " + id));
+         existingTask.setTitle(taskDetails.getTitle());
+         existingTask.setDescription(taskDetails.getDescription());
+         existingTask.setStatus(taskDetails.getStatus());
+         return taskRepository.save(existingTask);
+     }
+
+     @Override
+    public void deleteTask(Long id) {
+         Task existingTask = taskRepository.findById(id)
+                 .orElseThrow(() -> new RuntimeException("Görev bulunamadı! ID: " + id));
+
+         taskRepository.delete(existingTask);
+     }
+
 }
