9757247046a8b2c96fe47567ee0739e2aea00c3c
Alp Efe Şekerci <alpefesekerci@gmail.com>
2026-06-09 10:17:32 +0300

    feat: add initial spring boot backend setup

---
+++ backend/src/main/java/com/alpefesekerci/trello_clone_app/TrelloCloneAppApplication.java
@@ -0,0 +1,13 @@
+package com.alpefesekerci.trello_clone_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrelloCloneAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrelloCloneAppApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/alpefesekerci/trello_clone_app/TrelloCloneAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alpefesekerci.trello_clone_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrelloCloneAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
